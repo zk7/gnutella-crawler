0fb25e249f2d3c6930fb019ff333801c57361fe8
zk7 <zk7@users.noreply.github.com>
2014-03-11 01:45:15 -0500

    Create common.h

---
+++ common.h
@@ -0,0 +1,16 @@
+// gnutella crawler
+// Author: Zain Shamsi
+
+#pragma once
+#include <stdio.h>
+#include <Windows.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <map>
+#include "Peer.h"
+using namespace std;
+
+const int DEBUG_ON = 0;
